d0624fba0cd26dad63c1ab7b7a6084ee65336434
Bervas Yahn <bervasyahn@gmail.com>
2022-11-16 23:34:46 +0100

    Ajout du calcul du taux de réussite du décryptage

--- CA.py
+++ CA.py
@@ -20,7 +20,7 @@ class CA(User.User) :
     def __init__(self) :
         User.User.__init__(self)
     
-    def generateCertificate(self, mC, footprintC, publicKey) :
+    def generateCertificate(self, user, mC, footprintC) :
         """
         Return the certificate if the footprint is the same as the decrypted version
 
@@ -35,17 +35,17 @@ class CA(User.User) :
         footprintc : long
             The footprint of the crypted messages
         """
-        print("Le message crypté : " + str(mC))
-        footprintD = (self.decryption(publicKey, publicKey[0], footprintC[0]),
-                        self.decryption(publicKey, publicKey[0], footprintC[1]))
+        print("La clé cryptée : " + str(mC))
+        footprintD = (user.decryption(footprintC[0], user.publicKey[0]),
+                        user.decryption(footprintC[1], user.publicKey[0]))
         print("L'empreinte décryptée : " + str(footprintD))
-        mD = (self.decryption(self.publicKey, self.privateKey, mC[0]),
-                self.decryption(self.publicKey, self.privateKey, mC[1]))
-        print("Le message décrypté : " + str(mD))
+        mD = (self.decryption(mC[0], self.privateKey),
+                self.decryption(mC[1], self.privateKey))
+        print("La clé décrypté : " + str(mD))
         footprint = (self.footprint(mD[0]), 
                         self.footprint(mD[1]))
-        print("L'empreinte du message décrypté : " + str(footprint))
+        print("L'empreinte de la clé décryptée : " + str(footprint))
         if (footprint[0] == footprintD[0] and footprint[1] == footprintD[1]) :
-            return self.encryption(publicKey[0], self.publicKey, self.privateKey)
+            return [self.encryption(user.publicKey[0], self.privateKey), self.encryption(user.publicKey[1], self.privateKey)]
         else :
             return None
--- Mini_RSA.py
+++ Mini_RSA.py
@@ -2,33 +2,31 @@ import os
 import User
 import CA
 
-alice = User.User()
-bob = User.User()
-ca = CA.CA()
-while(ca.publicKey[0] < alice.publicKey[0] and ca.publicKey[1] < alice.publicKey[1]) :
+cpt = 0
+nb = 100
+
+for i in range(nb) :
+    alice = User.User()
+    bob = User.User()
     ca = CA.CA()
-print(alice.publicKey)
-#print(ca.publicKey)
+    while(ca.publicKey[0] < alice.publicKey[0] and ca.publicKey[1] < alice.publicKey[1]) :
+        ca = CA.CA()
 
-messageAuCA = alice.publicKey
-messageAuCA = (ca.encryption(messageAuCA[0], ca.publicKey[0]), 
-                ca.encryption(messageAuCA[1], ca.publicKey[0]))
-aliceKeyHasher = (User.User.footprint(alice.publicKey[0]), 
-                    User.User.footprint(alice.publicKey[1]))
-print("L'empreinte du message : " + str(aliceKeyHasher))
-messageSigner = (alice.encryption(aliceKeyHasher[0], alice.privateKey), 
-                alice.encryption(aliceKeyHasher[1], alice.privateKey))
-messageEnvoyer = (messageAuCA, messageSigner)
+    print("La clé à certifier : " + str(alice.publicKey))
 
-print("Le message envoyer : " + str(messageEnvoyer))
+    cryptMessage = (ca.encryption(alice.publicKey[0], ca.publicKey[0]), 
+                    ca.encryption(alice.publicKey[1], ca.publicKey[0]))
+    aliceFootprint = (User.User.footprint(alice.publicKey[0]), 
+                        User.User.footprint(alice.publicKey[1]))
+    cryptAliceFootprint = (alice.encryption(aliceFootprint[0], alice.privateKey), 
+                    alice.encryption(aliceFootprint[1], alice.privateKey))
 
-messageRecu = (ca.decryption(messageEnvoyer[0][0], ca.privateKey), 
-                ca.decryption(messageEnvoyer[0][1], ca.privateKey))
-EmpreinteDechiffree = (alice.decryption(messageEnvoyer[1][0], alice.publicKey[0]), 
-                        alice.decryption(messageEnvoyer[1][1], alice.publicKey[0]))
+    alice.certificate = ca.generateCertificate(alice, cryptMessage, cryptAliceFootprint)
+    print("La clé certifiée : " + str(alice.certificate))
 
-print("Le message recu est : " + str(messageRecu))
-print("L'empreinte dechiffrée : " + str(EmpreinteDechiffree))
-print("L'empreinte du message recu : " + str(User.User.footprint(messageRecu[0])) + " " + str(User.User.footprint(messageRecu[1])))
-verificationHashage = User.User.footprint(messageRecu[0]) == EmpreinteDechiffree[0] and User.User.footprint(messageRecu[1]) == EmpreinteDechiffree[1]
-print(verificationHashage)
+    if (alice.certificate != None) :
+        cpt += 1
+
+print(" ")
+
+print("Taux de réussite du décryptage : " + str((cpt / nb) * 100) + "%")
